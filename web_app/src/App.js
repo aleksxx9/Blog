4125bc677cb5b0958b0e5017aa4c257103e56ad1
Aleksandras <aleksxx98@gmail.com>
2020-01-15 23:54:19 +0200

    New react app
    New react app created with router library and materialise library

---
+++ web_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+1234
+    </div>
+  );
+}
+
+export default App;
